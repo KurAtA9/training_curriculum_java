e86150bb942afcd20e273e54a531cd16befad80d
KurAtA9 <kurata@ibis-japan.co.jp>
2026-05-21 16:34:12 +0900

    Issue6

--- src/main/java/in/tech_camp/training_curriculum_java/controller/CalendarsController.java
+++ src/main/java/in/tech_camp/training_curriculum_java/controller/CalendarsController.java
@@ -65,10 +65,15 @@ public class CalendarsController {
               todayPlans.add(plan.getPlan());
           }
       }
+      int wdayNum = todaysDate.getDayOfWeek().getValue();
+      if(wdayNum >= 7){
+        wdayNum = wdayNum -7;
+      }
 
       dayMap.put("month", currentDate.getMonthValue());
       dayMap.put("date", currentDate.getDayOfMonth());
       dayMap.put("plans", todayPlans);
+      dayMap.put("wday", wdays[wdayNum]);
 
       weekDays.add(dayMap);
     }
